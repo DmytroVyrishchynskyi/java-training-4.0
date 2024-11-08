76c7cb58162daf7dec75e8bd7d3a5d03af853355
DmytroVyrishchynskyi <magic.scholz@gmail.com>
2024-11-08 19:43:20 +0100

    test

--- src/StreamTask.java
+++ src/StreamTask.java
@@ -1,12 +1,18 @@
+import java.util.Arrays;
+
 public class StreamTask {
 
     public static int getSumSquareEven(int[] numbers) {
-        // TODO replace return with your code
-        return 0;
+        return Arrays.stream(numbers)
+                .filter(n -> n % 2 == 0)     // Filter only even numbers
+                .map(n -> n * n)            // Square each even number
+                .sum();                     // Sum all of squared values
     }
 
     public static int[] getSortedOddsArray(int[] numbers) {
-        // TODO replace return with your code
-        return new int[]{};
+        return Arrays.stream(numbers)
+                .filter(n -> n % 2 != 0)    // Filter only odd numbers
+                .sorted()                   // Sort in ascending order
+                .toArray();                 // Convert to int array
     }
 }
